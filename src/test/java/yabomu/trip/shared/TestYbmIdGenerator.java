a889260fdef29fd678f88f8d548bb48829e517bd
takku66 <tattaka15@gmail.com>
2021-09-30 22:32:56 +0900

    スレッドについて認識不足だった。テスト修正

--- src/test/java/yabomu/trip/shared/TestYbmIdGenerator.java
+++ src/test/java/yabomu/trip/shared/TestYbmIdGenerator.java
@@ -18,28 +18,31 @@ class TestYbmIdGenerator {
 
 	/**
 	 * <pre>
+	 * <s>
 	 * スレッドの生成数によって、テストが失敗することがある
 	 * CPUのコア数とかの問題？
 	 * thnumが4までなら安定しているが、5になると赤になることがある。
 	 * おそらく自CPUが2コア4論理プロセッサのためか。。。
 	 * ただし生成されるIDの重複は発生していない様子
+	 * </s>
+	 * 生成したスレッドをプールしてjoinしていたが、
+	 * スレッドの処理実行後にjoinするようにしたらうまくいった
+	 * スレッドの生成処理と実行処理、待機処理の順序が整理できていなかった
 	 * </pre>
 	 */
 	@Test
 	void test() {
 
-		int thnum = 4;
-		int loopnum = 1000;
+		int thnum = 25;
+		int loopnum = 5000;
 
 		Thread[] ts = new Thread[thnum];
 		Map<Long, Long> idMap = new HashMap<>();
-		for(int i = 0; i < thnum; i++) {
-			ts[i] = new Thread(new MultiThreadTest(idMap, loopnum));
-			ts[i].start();
-		}
 		try {
 			for(int i = 0; i < thnum; i++) {
-					ts[i].join();
+				ts[i] = new Thread(new MultiThreadTest(idMap, loopnum));
+				ts[i].start();
+				ts[i].join();
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
